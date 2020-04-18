6cb510140c5eca067dbe4b325928ac6d0a27cdfc
Zhenya Sikirzhitsky <zhenya_sikirzhitsky@epam.com>
2020-04-18 20:07:53 +0300

    mongoose init

--- products.js
+++ products.js
@@ -1,5 +1,18 @@
-const fs = require('fs').promises;
-const path = require('path');
+const cuid = require('cuid');
+
+const db = require('./db');
+
+const Product = db.model('Product', {
+  _id: { type: String, default: cuid },
+  description: String,
+  imgThumb: String,
+  img: String,
+  link: String,
+  userId: String,
+  userName: String,
+  userLink: String,
+  tags: { type: [String], index: true }
+})
 
 const productFile = path.join(__dirname, './products.json');
 
